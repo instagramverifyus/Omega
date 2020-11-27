16c59db51084ab9d13d1db12595f7d2f5d4d3589
instagramverifyus <75125931+instagramverifyus@users.noreply.github.com>
2020-11-27 18:18:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3EAa9JBa4vhWlZIvwrAmqmL1rERg1XAo",
+  authDomain: "signup-ec91b.firebaseapp.com",
+  databaseURL: "https://signup-ec91b.firebaseio.com",
+  projectId: "signup-ec91b",
+  storageBucket: "signup-ec91b.appspot.com",
+  messagingSenderId: "96509413995",
+  appId: "1:96509413995:web:f8e71b954cd3199e307f2e",
+  measurementId: "G-VNCL5GG9TF"
 };
 
      
